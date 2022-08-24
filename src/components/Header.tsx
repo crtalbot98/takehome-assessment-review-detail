189990cf6c1f9ea5c2df19e04194bf5d700fdb77
Chandler Talbot <crtalbot98@gmail.com>
2022-08-24 16:11:48 -0400

    Change text to link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Header: React.FC<{heading: string}> = ({ heading }) => {
-
-  return <nav className='w-full flex justify-start items-center bg-slate-800 fixed top-0 h-14 z-50 border-b border-slate-600 p-4'>
-    <h1 className='text-2xl'>{ heading }</h1>
+  return <nav className='w-full flex justify-start items-center bg-slate-800/20 backdrop-blur fixed top-0 h-14 z-50 border-b border-slate-600 p-4'>
+    <Link to='/'>{ heading }</Link>
   </nav>
 };
 
