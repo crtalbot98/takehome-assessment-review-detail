7aad04365d08c4817b3ad7088a503583e44600e9
Chandler Talbot <crtalbot98@gmail.com>
2022-08-24 16:11:21 -0400

    Changed from BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from "react-router-dom";
 import App from "./components/App";
 
 const container = document.querySelector('#app');
 const root = createRoot(container!);
 
 root.render(
-  <App/>
+  <HashRouter>
+    <App/>
+  </HashRouter>
 );
