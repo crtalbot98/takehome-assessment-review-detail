a58b31b0efe41d248564cc977ee8d59f5ac212b4
Chandler Talbot <crtalbot98@gmail.com>
2022-08-26 18:44:13 -0400

    added state to navigation

--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { IReview } from './Review';
+import { reviewContext } from './App';
 
-const ReviewList: React.FC<{reviews: IReview[]}> = ({ reviews }) => {
+const ReviewList: React.FC = () => {
   const navigate = useNavigate();
-  const loc = useLocation();
-  console.log(reviews)
+  const { reviews } = useContext(reviewContext);
+  
+  const selectReview = (review: IReview) => {
+    navigate(`/${review.id}`, { state: { reviewId: review.id } })
+  }
+  
   const reviewElms = reviews.map((review) =>
     <div 
       key={review.id}
       className='w-10/12 md:w-72 h-56 flex flex-col bg-slate-800/40 backdrop-blur border border-slate-600 text-ellipsis p-3 rounded cursor-pointer subpixel-antialiased hover:scale-105 hover:bg-slate-800/100 hover:backdrop-blur-none'
-      onClick={() => navigate(`/${review.id}`)}
+      onClick={() => selectReview(review) }
     >
       <h2 className='text-xl'>{review.place}</h2>
       <p className='absolute self-end'>{review.rating}/5</p>
       <h3>{review.author}</h3>
+      <p>{new Date(review.published_at).toLocaleString()}</p>
       <p className='line-clamp-4 mt-auto'>{review.content}</p>
     </div>
   );
