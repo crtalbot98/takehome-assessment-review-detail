1df83425595ee9aec8bfabd341114cbe9f6bc317
Chandler Talbot <crtalbot98@gmail.com>
2022-08-26 18:42:35 -0400

    add localStorage and updated routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { createContext, Dispatch, SetStateAction, useEffect } from 'react';
 import ReviewList from './ReviewList';
 import Header from './Header';
-import { IReview, Review } from './Review';
+import type { IReview } from './Review';
+import ReviewPage from './ReviewPage';
 import NotFound from './NotFound';
 import { useRoutes } from 'react-router-dom';
 import type { RouteObject } from "react-router-dom";
 import '../styles/main.css';
 import useReviews from '../hooks/useReviews';
 
-const App: React.FC = () => {
-  const [reviews] = useReviews();
+interface IReviewContext {
+  reviews: IReview[]
+  setReviews: Dispatch<SetStateAction<IReview[]>>
+}
+
+export const reviewContext = createContext<IReviewContext>(
+  { reviews: [], setReviews: () => {} }
+);
+
+export const App: React.FC = () => {
+  const [reviews, setReviews] = useReviews();
   const routes: RouteObject[] = [
     { path: '/',
       children: [
-        { index: true, element: <ReviewList reviews={reviews as IReview[]}/> },
-        { path: ':id', element: <Review/> }
+        { index: true, element: <ReviewList/> },
+        { path: ':id', element: <ReviewPage/> }
       ]
     },
     { path: '*', element: <NotFound/> },
   ]
   const routedElm = useRoutes(routes);
 
+  useEffect(() => {
+    if(!reviews) return;
+    localStorage.setItem('reviews', JSON.stringify(reviews))
+  }, [reviews]);
+
   return <div className='min-h-screen bg-gradient-to-bl from-slate-900 via-slate-900 to-indigo-900 text-slate-200 flex flex-col justify-start items-center space-y-8 py-20'>
     <Header heading='Reviews'/>
-    { routedElm }
+    <reviewContext.Provider value={{ reviews: reviews, setReviews: setReviews }}>
+      { routedElm }
+    </reviewContext.Provider>
   </div>
 };
-
-export default App;
--- src/hooks/useReviews.ts
+++ src/hooks/useReviews.ts
@@ -2,19 +2,16 @@ import { useState, useEffect, SetStateAction, Dispatch } from 'react';
 import reviewJson from '../data/reviews.json';
 import { IReview } from 'src/components/Review';
 
-const useReviews = (id: string | null = null): [IReview | IReview[], Dispatch<SetStateAction<IReview | IReview[]>>] => {
-  const [reviews, setReviews] = useState<IReview[] | IReview>([]);
+const useReviews = (): [IReview[], Dispatch<SetStateAction<IReview[]>>] => {
+  const [reviews, setReviews] = useState<IReview[]>([]);
   
   useEffect(() => {
     const reviewData: IReview[] = JSON.parse(JSON.stringify(reviewJson));
+    const localReviews = localStorage.getItem('reviews');
 
-    if(id) {
-      const selectedIndex = reviewData.findIndex((review) => id === review.id);
-      if(selectedIndex !== -1) setReviews(reviewData[selectedIndex]);
-      return
-    }
-
-    setReviews(reviewData)
+    if(!localReviews || localReviews.length < 1) localStorage.setItem('reviews', JSON.stringify(reviewData));
+    if(localReviews) setReviews(JSON.parse(localReviews));
+    else setReviews(reviewData)
   }, []);
 
   return [ reviews, setReviews ]
